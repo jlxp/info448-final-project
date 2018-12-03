6ebe93afcba557f76afa42aaaf83762d68f91060
jlxp <joylxphillips@gmail.com>
2018-12-03 13:50:23 -0800

    settings activity created and connected to tab activity

--- SeattleSpotHunt/app/src/main/java/edu/uw/maps101/seattlespothunter/TabActivity.kt
+++ SeattleSpotHunt/app/src/main/java/edu/uw/maps101/seattlespothunter/TabActivity.kt
@@ -1,5 +1,6 @@
 package edu.uw.maps101.seattlespothunter
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
@@ -47,5 +48,18 @@ class TabActivity : AppCompatActivity() {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_tab, menu)
         return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        when (item.itemId) {
+            R.id.action_settings -> {
+                startActivity(Intent(this, SettingsActivity::class.java))
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
     }
 }
