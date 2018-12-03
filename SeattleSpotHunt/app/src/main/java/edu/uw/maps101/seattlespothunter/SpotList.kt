44d822ad7b309e49e5027277f537dff299a194eb
Madeleine Stevenson <maps101@uw.edu>
2018-12-03 13:46:36 -0800

    Spots allocated and latlng added

--- SeattleSpotHunt/app/src/main/java/edu/uw/maps101/seattlespothunter/SpotList.kt
+++ SeattleSpotHunt/app/src/main/java/edu/uw/maps101/seattlespothunter/SpotList.kt
@@ -1,26 +1,45 @@
 package edu.uw.maps101.seattlespothunter
 
+import android.util.Log
 import com.google.android.gms.maps.model.LatLng
 
 class SpotList: ArrayList<Spot>() {
     val list = ArrayList<Spot>()
 
     init {
-        addAllSpots()
+        addAllSpots() //initalize list
     }
 
 
     //This helper function adds 20 new spot objects to the Array list for each of the predetermined tourists spots and intializes all of them to
     private fun addAllSpots() {
-        list.add(Spot(R.strings.spot1name, R.strings.spot1desc, false, LatLng(R.locations.spot1lat, R.location))) //Space Needle
-        //Space Needle
-        //Pike Place
-        //Pacific Science Center
-        //MoPop
-        //Living Computer Musuem
-        //UW
-        //Seattle Art Musesum
-        //Chehuly
+        val name = get(R.string.spot1name)
+        Log.v("SpotList.name", get(R.string.spot1name).name)
+        Log.v("SpotList as String", get(R.string.spot1name) as String)
+        Log.v("SpotList.toString()", get(R.string.spot1name).toString())
+
+        // 1 Space Needle LatLng(47.6206537, -122.3487383)
+        list.add(Spot(name, get(R.string.spot1desc) as String, false, LatLng(0.0, 0.0)))
+        // 2 Pike Place Market LatLng(47.608987, -122.340682)
+        // 3 Museum of Pop Culture LatLng(47.621492, -122.348123)
+        // 4 Chihuly Garden and Glass LatLng(47.620553, -122.350455)
+        // 5 Seattle Art Mueseum LatLng(47.607701,-122.338558)
+        // 6 Kerry Park LatLng(47.629471,-122.359924)
+        // 7 Woodland Park Zoo LatLng(47.669258,-122.350906)
+        // 8 Seattle Aquarium LatLng(47.603230,-122.330276)
+        // 9 Gas Works Park LatLng(47.645599,-122.334930)
+        // 10 Gum Wall LatLng(47.608124,-122.340157)
+        // 11 Pacific Science Center LatLng(47.619340,-122.350778)
+        // 12 Seattle Great Wheel LatLng(47.606220, -122.342517)
+        // 13 Fremont Troll LatLng(47.651048,-122.347235)
+        // 14 Golden Gardens Park LatLng(47.692200,-122.402980)
+        // 15 Olympic Sculpture Park LatLng(47.616238,-122.354313)
+        // 16 Washington Park Arboretum | UW Botanic Gardens LatLng(47.635185,-122.296471)
+        // 17 Smith Tower LatLng(47.601958,-122.331738)
+        // 18 University of Washington - Seattle Campus LatLng(47.655923, -122.309345)
+        // 19 Seattle Japanese Gardens LatLng(47.629839,-122.297179)
+        // 20 Coleman Dock LatLng(47.601716,-122.336667)
+
     }
 
 }
